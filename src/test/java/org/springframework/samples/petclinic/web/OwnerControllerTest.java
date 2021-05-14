f9a4350b093c254e9f6b7cd43db972991f69b016
PierreSQS <nomo_messina@web.de>
2021-05-14 21:30:53 +0200

    Chap161: Assignment- Test GetMethod with Spring MVC Test.

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
@@ -16,8 +23,26 @@ class OwnerControllerTest {
 
 
     @Test
-    void tempTest() {
-
+    void initFindFormTest() throws Exception {
         //ownerController
+        MockMvc mockMvc = standaloneSetup(ownerController).build();
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name("owners/findOwners"))
+                .andDo(print());
+
+    }
+
+    @Test
+    void initCreationFormTest() throws Exception {
+        //ownerController
+        MockMvc mockMvc = standaloneSetup(ownerController).build();
+        mockMvc.perform(get("/owners/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"))
+                .andDo(print());
+
     }
 }
