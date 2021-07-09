0af99996edefe525bd992bab4c6c04744b2589e8
PierreSQS <nomo_messina@web.de>
2021-07-09 22:15:19 +0200

    Chap154-Assignment: Added Test, properties file and new WordProducer

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/PropertiesLaurelTest.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("laurel-properties")
+@SpringJUnitConfig(PropertiesLaurelTest.TestConfig.class)
+@TestPropertySource("classpath:laurel.properties")
+public class PropertiesLaurelTest {
+
+    @Profile("laurel-properties")
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void hearingInterpreter() {
+        String word = hearingInterpreter.whatIheard();
+
+        assertEquals("LAUREL", word);
+
+    }
+}
